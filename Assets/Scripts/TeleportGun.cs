cefae2c8f9bb4a204cba97cb33c46e9fe93ff038
EricWehrly <eric@disruptiontheory.com>
2019-08-03 18:47:04 -0400

    body snatchin

--- Assets/Scripts/TeleportGun.cs
+++ Assets/Scripts/TeleportGun.cs
@@ -1,13 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
 
 public class TeleportGun : MonoBehaviour
 {
+    public bool EnableDebugLines = true;
+    public bool EnableTeleport = true;
+    public string TagFilter = null;
+
     private Gradient gradient;
     private RigidbodyFirstPersonController m_CharacterController;
 
-    public bool DrawLines = true;
-    public bool ShouldTeleport = true;
+    // TODO: initial 'mesh' for player entering level ...
+    private GameObject lastTarget;
 
     private void Awake()
     {
@@ -19,7 +24,7 @@ public class TeleportGun : MonoBehaviour
             new GradientColorKey[] { new GradientColorKey(Color.red, 0.0f), new GradientColorKey(Color.red, 1.0f) },
             new GradientAlphaKey[] { new GradientAlphaKey(alpha, 0.0f), new GradientAlphaKey(alpha, 1.0f) }
         );
-        
+
         m_CharacterController = GetComponent<RigidbodyFirstPersonController>();
     }
 
@@ -34,23 +39,49 @@ public class TeleportGun : MonoBehaviour
             {
                 DrawLine(this.transform.position, hit.point);
 
-                if (ShouldTeleport)
+                if (ShouldTeleport(ref hit))
                 {
+                    if (lastTarget != null)
+                    {
+                        lastTarget.gameObject.SetActive(true);
+                    }
+
+                    StealBody(hit.transform.gameObject);
+
                     // TODO: we want to actually step the ray back from the hit point,
                     // as this spawns the player 'closer' to what is hit than where they'd normally collide
-                    m_CharacterController.enabled = false;
-                    this.transform.position = hit.point;
-                    m_CharacterController.enabled = true;
+                    TeleportTo(hit.point);
                 }
             }
         }
     }
 
+    private bool ShouldTeleport(ref RaycastHit hit)
+    {
+        return TagFilter != null
+            && hit.transform.tag == TagFilter
+            && EnableTeleport;
+    }
+
+    private void StealBody(GameObject target)
+    {
+        lastTarget = target;
+        target.SetActive(false);
+    }
+
+    private void TeleportTo(Vector3 point)
+    {
+        m_CharacterController.enabled = false;
+        this.transform.position = point;
+        m_CharacterController.enabled = true;
+    }
+
     private void DrawLine(Vector3 pointOne, Vector3 pointTwo)
     {
-        if (!DrawLines) return;
+        if (!EnableDebugLines) return;
 
         GameObject newLine = new GameObject();
+        newLine.name = "Debug Line";
         LineRenderer lineRenderer = newLine.AddComponent<LineRenderer>();
         lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
         lineRenderer.widthMultiplier = 0.2f;

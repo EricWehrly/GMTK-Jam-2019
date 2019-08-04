4aa65211f73fcf0959fd8d330de70808a4950289
Sean White <seanwhite211@gmail.com>
2019-08-04 15:00:53 -0400

    add script to trigger gun fire sound

---
+++ Assets/AudioScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioScript : MonoBehaviour
+{
+    AudioSource gunfire;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gunfire = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            gunfire.Play();
+    }
+}
